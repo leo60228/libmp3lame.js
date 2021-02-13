f4eb22b1dad46e09c34628dc7dc099e42ccc4b10
leo60228 <leo@60228.dev>
2021-02-12 19:43:49 -0500

    lame package

--- flake.nix
+++ flake.nix
@@ -7,8 +7,39 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in rec {
+        packages = {
+          lame = (pkgs.lame.override {
+            nasmSupport = false;
+            nasm = null;
+            cpmlSupport = false;
+            analyzerHooksSupport = false;
+            decoderSupport = false;
+            frontendSupport = false;
+            stdenv = pkgs.emscriptenStdenv;
+          }).overrideAttrs (oldAttrs: {
+            configurePhase = ''
+            HOME=$TMPDIR
+            runHook preConfigure
+
+            emconfigure ./configure --prefix=$out $configureFlags CFLAGS=-O2
+
+            runHook postConfigure
+            '';
+            postBuild = ''
+            pushd libmp3lame/.libs/
+            emcc -O2 ./libmp3lame.so -o ./libmp3lame.js
+            popd
+            '';
+            outputs = [ "out" ];
+            installPhase = ''
+            mkdir -p $out/lib
+            cp libmp3lame/.libs/*.js libmp3lame/.libs/*.wasm $out/lib/
+            '';
+            doCheck = false;
+          });
+        };
         devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ nodejs-14_x emscripten ];
+          nativeBuildInputs = with pkgs; [ nodejs-14_x ];
         };
       }
     );
