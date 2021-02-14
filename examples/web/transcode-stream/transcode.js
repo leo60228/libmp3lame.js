a74246b1808f6769fc333131a6537ba850002154
leo60228 <leo@60228.dev>
2021-02-13 20:31:28 -0500

    transcode-stream

---
+++ examples/web/transcode-stream/transcode.js
@@ -0,0 +1,44 @@
+import LAMEConfig from '../../../lib/index.js';
+import AudioFeeder from 'audio-feeder';
+
+async function transcode(data) {
+    const audioCtx = new AudioContext({ sampleRate: 44100 });
+
+    console.time('decoding');
+    const audio = await audioCtx.decodeAudioData(data);
+    console.timeEnd('decoding');
+
+    let feeder = new AudioFeeder();
+    feeder.init(2, 44100);
+
+    let encoder = new Worker('worker.js');
+    let started = false;
+
+    encoder.onmessage = async (evt) => {
+        const encodedBuffer = evt.data;
+
+        console.time('decoding');
+        const encodedAudio = await audioCtx.decodeAudioData(encodedBuffer);
+        console.timeEnd('decoding');
+
+        feeder.bufferData([audio.getChannelData(0), audio.getChannelData(1)]);
+
+        if (!started) {
+            feeder.start();
+            started = true;
+        }
+    };
+
+    encoder.postMessage([audio.getChannelData(0), audio.getChannelData(1)]);
+}
+
+document.getElementById('file-input').addEventListener('change', evt => {
+    const file = evt.target.files[0];
+    if (!file) return;
+    let reader = new FileReader();
+    reader.onload = evt => {
+        const data = evt.target.result;
+        transcode(data);
+    };
+    reader.readAsArrayBuffer(file);
+});
---
+++ examples/web/transcode-stream/worker.js
@@ -0,0 +1,36 @@
+import LAMEConfig from '../../../lib/index.js';
+
+const chunkSize = 44100 * 10;
+
+onmessage = async (evt) => {
+    const [left, right] = evt.data;
+
+    const chunks = Math.ceil(left.length / chunkSize);
+
+    for (let i = 0; i < chunks; i++) {
+        const start = i * chunkSize;
+        const end = Math.min((i + 1) * chunkSize, left.length);
+
+        const leftChunk = left.subarray(start, end);
+        const rightChunk = right.subarray(start, end);
+
+        let config = new LAMEConfig();
+        config.setChannels(2);
+        config.setInputSampleRate(44100);
+        config.setOutputSampleRate(22050);
+        config.setBitrate(48);
+
+        let lame = await config.build(1024 * 1024 * 10, 'libmp3lame.wasm');
+
+        console.time('encoding');
+        console.log('encoding');
+        lame.encodeFloats(leftChunk, rightChunk);
+        lame.flush();
+        console.timeEnd('encoding');
+
+        const encoded = lame.buffer();
+        const encodedBuffer = encoded.buffer.slice(encoded.byteOffset, encoded.byteOffset + encoded.byteLength);
+
+        postMessage(encodedBuffer);
+    }
+};
